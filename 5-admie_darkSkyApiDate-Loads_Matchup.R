092883a6dc780da4a64c681324c4439170988210
praxitelisk <praxitelisk@gmail.com>
2017-03-22 11:14:49 +0200

    minor changes, for renaming a column

---
+++ 5-admie_darkSkyApiDate-Loads_Matchup.R
@@ -0,0 +1,29 @@
+#####
+##Adding an extra date column to loads in order to match them with
+##darkSkyAPI dates anda meteorogical data.
+#####
+
+library(tibble) #for add_column command
+
+
+time = paste(myLoadsCrudeForm$DATE," ",myLoadsCrudeForm$HOUR,":00", sep="", collapse = NULL)
+time = strptime(time, format="%Y-%m-%d %H:%M")
+time = as.POSIXct(time)
+
+#an auxilliary variable taken from darkSkyAPI in order to check if
+#previous time variable is made correctly.
+darkSkyTime = darkSky.HistoricalData$time
+
+
+#add time column to loads data frame.
+myLoadsCrudeForm = add_column(myLoadsCrudeForm, time, .after = "HOUR")
+
+
+#renaming the long load columns into a shorter one
+names(myLoadsCrudeForm)[4] <- "Loads"
+
+
+#check it if works
+#darkSkyTime[2] == time[1]
+
+rm(time,darkSkyTime)
