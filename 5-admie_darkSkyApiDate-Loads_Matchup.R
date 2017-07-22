20caf294b1e385f9808df71133d557821356171e
praxitelisk <praxitelisk@gmail.com>
2017-07-22 10:41:17 +0300

    Adding some comments and minor modifications

--- 5-admie_darkSkyApiDate-Loads_Matchup.R
+++ 5-admie_darkSkyApiDate-Loads_Matchup.R
@@ -1,18 +1,16 @@
-#####
-##Adding an extra date column to loads in order to match them with
-##darkSkyAPI dates anda meteorogical data.
-#####
+###Adding an extra date column to loads in order to match them with
+#darkSkyAPI dates and meteorogical data.
 
 library(tibble) #for add_column command
 
 
 time = paste(myLoads$DATE," ",myLoads$HOUR,":00", sep="", collapse = NULL)
-time = strptime(time, format="%Y-%m-%d %H:%M")
-time = as.POSIXct(time)
+#time = strptime(time, format="%Y-%m-%d %H:%M")
+time = as.POSIXct(time, format="%Y-%m-%d %H:%M", tz="Europe/Athens")
 
 #an auxilliary variable taken from darkSkyAPI in order to check if
 #previous time variable is made correctly.
-darkSkyTime = darkSky.WeatherData$time
+#darkSkyTime = darkSky.WeatherData$time
 
 
 #add time column to loads data frame.
@@ -20,7 +18,7 @@ myLoads = add_column(myLoads, time, .after = "HOUR")
 
 
 #renaming the long load columns into a shorter one
-names(myLoads)[4] <- "Loads"
+#names(myLoads)[4] <- "Loads"
 
 
 #check it if works
