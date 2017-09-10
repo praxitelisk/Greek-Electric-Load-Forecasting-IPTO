b960d3e97dc35d836419a468c181caefcb615959
praxitelisk <praxitelisk@gmail.com>
2017-09-10 20:07:16 +0300

    rule based script with full features and default parameters for the classifier

---
+++ scripts/7.6.1-ipto_fitting_loads_rules_models_with_full_features.R
@@ -0,0 +1,133 @@
+####################################################################
+#####Rules with full features and default parameters################
+####################################################################
+
+
+library("Cubist")
+
+
+startTime <- proc.time()[3]
+
+#creating the train and test set splits####
+splitEvalSet = 365
+splitTestSet = splitEvalSet + 365
+len = dim(final.Data.Set)[1]
+
+#trainPart = floor(split * dim(final.Data.Set)[1])
+trainSet = final.Data.Set[1:(len - splitTestSet), ]
+evaluationSet = final.Data.Set[(len-splitTestSet + 1):(len - splitEvalSet), ]
+train.and.evalSet = final.Data.Set[1:(len - splitEvalSet), ]
+testSet = final.Data.Set[(len - splitEvalSet + 1):len, ]
+
+
+#if (!exists("best.rule.parameters.full")) {
+best.rule.parameters.full = list()
+best.rule.fit.full = list()
+best.rule.prediction.full = list()
+#}
+
+mape.rule.full.def = list()
+mae.rule.full.def = list()
+rmse.rule.full.def = list()
+mse.rule.full.def = list()
+prediction.rule.full.def = list()
+fit.rule.full.def = list()
+
+
+for(i in 1:24) {
+  
+  list.of.features = full.list.of.features
+
+  
+  cat("\n\n training rules model: Load.",i-1, "\n", sep="")
+  
+  #create the predictor variables from training
+  FeaturesVariables =
+    train.and.evalSet[list.of.features]
+  
+  #add the response variable in trainSet
+  FeaturesVariables[paste("Loads", i-1, sep=".")] = 
+    train.and.evalSet[paste("Loads", i-1, sep=".")]
+  
+  
+  set.seed(123)
+  assign(paste("fit.rule", i-1, sep="."), 
+         cubist(x = FeaturesVariables[-grep(paste("^Loads", i-1, sep="."), names(FeaturesVariables))], y = FeaturesVariables[[paste("Loads", i-1, sep=".")]]))
+  
+  
+  FeaturesVariables[paste("Loads", i-1, sep=".")] = NULL
+  
+  
+  FeaturesVariables =
+    train.and.evalSet[list.of.features]
+  
+  
+  predictor.df = data.frame()
+  predictor.df = FeaturesVariables[0, ]
+  predictor.df = rbind(predictor.df, testSet[names(testSet) %in% names(predictor.df)])
+  
+  
+  assign(paste("prediction.rule", i-1, sep="."), predict(get(paste("fit.rule",i-1,sep=".")), predictor.df))
+  
+  
+  #calculate mape
+  temp.mape = 100 * mean(unlist(abs((get("testSet")[paste("Loads", i-1, sep=".")] - get(paste("prediction.rule", i-1, sep=".")))/get("testSet")[paste("Loads", i-1, sep=".")])))
+  cat("mape.", i-1 ," = ", temp.mape,"\n\n", sep = "")
+  
+  
+  temp.mae =  mean(unlist(abs((get("testSet")[paste("Loads", i-1, sep=".")] - get(paste("prediction.rule", i-1, sep=".")))/get("testSet")[paste("Loads", i-1, sep=".")])))
+  
+  
+  temp.rmse = sqrt(mean(unlist(abs((get("testSet")[paste("Loads", i-1, sep=".")] - get(paste("prediction.rule", i-1, sep=".")))/get("testSet")[paste("Loads", i-1, sep=".")]))^2))
+  
+  
+  temp.mse = mean(unlist(abs((get("testSet")[paste("Loads", i-1, sep=".")] - get(paste("prediction.rule", i-1, sep=".")))/get("testSet")[paste("Loads", i-1, sep=".")]))^2)
+  
+  
+  fit.rule.full.def[[paste("fit.rule",i-1,sep=".")]] = get(paste("fit.rule",i-1, sep="."))
+  
+  prediction.rule.full.def[[paste("prediction.rule",i-1,sep=".")]] = get(paste("prediction.rule",i-1, sep="."))
+  
+  mape.rule.full.def[[paste("mape.rule",i-1,sep=".")]] = temp.mape
+  mae.rule.full.def[[paste("mae.rule",i-1,sep=".")]] = temp.mae
+  mse.rule.full.def[[paste("mse.rule",i-1,sep=".")]] = temp.mse
+  rmse.rule.full.def[[paste("rmse.rule",i-1,sep=".")]] = temp.rmse
+  
+  
+}
+
+
+#calculate the mean mape####
+cat("calculate the mean mape\n")
+mean.mape.rule.full.def = mean(unlist(mape.rule.full.def))
+
+cat("calculate the mean mae\n")
+mean.mae.rule.full.def = mean(unlist(mae.rule.full.def))
+
+cat("calculate the mean mse\n")
+mean.mse.rule.full.def = mean(unlist(mse.rule.full.def))
+
+cat("calculate the mean rmse\n")
+mean.rmse.rule.full.def = mean(unlist(rmse.rule.full.def))
+
+
+cat("mean rule mape: ", round(mean.mape.rule.full.def,3), "\n")
+cat("mean rule mae: ", round(mean.mae.rule.full.def,5), "\n")
+cat("mean rule mse: ", round(mean.mse.rule.full.def,5), "\n")
+cat("mean rule rmse: ", round(mean.rmse.rule.full.def,5), "\n")
+
+
+cat("elapsed time in minutes: ", (proc.time()[3]-startTime)/60,"\n")
+
+
+
+rm(list=ls(pattern="fit.rule.[0-9]"))
+rm(list=ls(pattern="prediction.rule.[0-9]"))
+rm(list=ls(pattern="mape.rule.[0-9]"))
+rm(list=ls(pattern="mae.rule.[0-9]"))
+rm(list=ls(pattern="mse.rule.[0-9]"))
+rm(list=ls(pattern="rmse.rule.[0-9]"))
+rm(list=ls(pattern="min.mape."))
+rm(list=ls(pattern="temp."))
+rm(i)
+
