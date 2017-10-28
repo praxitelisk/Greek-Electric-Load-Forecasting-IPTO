318b248deb4e56c8e108ae824e12cc3e477dbe73
praxitelisk <praxitelisk@gmail.com>
2017-10-28 09:32:43 +0300

    minor changes

--- scripts/6.1-ipto_adding_noise.R
+++ scripts/6.1-ipto_adding_noise.R
@@ -3,7 +3,7 @@ final.Data.Set = backUp.final.Data.Set
 
 #get current day features####
 current.day.list.of.features = 
-  colnames(final.Data.Set)[-grep("^thessaloniki.icon|^athens.icon|^thessaloniki.summary|^athens.summary|^isHoliday|^isWeekend|^isRushHour|^cosine.day.of.year.0|^sine.day.of.year|^day.of.year|^cosine.day.of.week|^sine.day.of.week|^day.of.week|^weekday|^time|^yesterday|^three|^two|^Loads|^thessaloniki.uvIndex|^athens.uvIndex", colnames(final.Data.Set))]
+  colnames(final.Data.Set)[-grep("^thessaloniki.icon|^athens.icon|^thessaloniki.summary|^athens.summary|^isHoliday|^isWeekend|^isRushHour|^cosine.day.of.year|^sine.day.of.year|^day.of.year|^cosine.day.of.week|^sine.day.of.week|^day.of.week|^weekday|^time|^yesterday|^three|^two|^Loads", colnames(final.Data.Set))]
 
 
 #change the names for current loads adding the prefix forecast.####
@@ -12,24 +12,24 @@ colnames(final.Data.Set)[grep(paste("^", current.day.list.of.features, collapse 
 
 
 #adding noise from uniform distribution####
-final.Data.Set[grep("^forecast", colnames(final.Data.Set))] = 
-  round(final.Data.Set[grep("^forecast", colnames(final.Data.Set))] +
-          runif(final.Data.Set[grep("^forecast", colnames(final.Data.Set))], -0.5, 0.5), 2)
+# final.Data.Set[grep("^forecast", colnames(final.Data.Set))] = 
+#   round(final.Data.Set[grep("^forecast", colnames(final.Data.Set))] +
+#           runif(final.Data.Set[grep("^forecast", colnames(final.Data.Set))], -0.5, 0.5), 2)
 
 
 #recalculate the sine and cosine from windbearing####
-for(i in 1:24) {
-  final.Data.Set[paste("forecast.sine.thessaloniki.windBearing", i-1, sep=".")] = 
-    sin(final.Data.Set[paste("forecast.thessaloniki.windBearing", i-1, sep=".")] * pi / 180)
-  
-  final.Data.Set[paste("forecast.cosine.thessaloniki.windBearing", i-1, sep=".")] = 
-    cos(final.Data.Set[paste("forecast.thessaloniki.windBearing", i-1, sep=".")] * pi / 180)
-  
-  final.Data.Set[paste("forecast.sine.athens.windBearing", i-1, sep=".")] = 
-    sin(final.Data.Set[paste("forecast.athens.windBearing", i-1, sep=".")] * pi / 180)
-  
-  final.Data.Set[paste("forecast.cosine.athens.windBearing", i-1, sep=".")] = 
-    cos(final.Data.Set[paste("forecast.athens.windBearing", i-1, sep=".")] * pi / 180)
-}
+# for(i in 1:24) {
+#   final.Data.Set[paste("forecast.sine.thessaloniki.windBearing", i-1, sep=".")] = 
+#     sin(final.Data.Set[paste("forecast.thessaloniki.windBearing", i-1, sep=".")] * pi / 180)
+#   
+#   final.Data.Set[paste("forecast.cosine.thessaloniki.windBearing", i-1, sep=".")] = 
+#     cos(final.Data.Set[paste("forecast.thessaloniki.windBearing", i-1, sep=".")] * pi / 180)
+#   
+#   final.Data.Set[paste("forecast.sine.athens.windBearing", i-1, sep=".")] = 
+#     sin(final.Data.Set[paste("forecast.athens.windBearing", i-1, sep=".")] * pi / 180)
+#   
+#   final.Data.Set[paste("forecast.cosine.athens.windBearing", i-1, sep=".")] = 
+#     cos(final.Data.Set[paste("forecast.athens.windBearing", i-1, sep=".")] * pi / 180)
+# }
 
 rm(current.day.list.of.features, i)
