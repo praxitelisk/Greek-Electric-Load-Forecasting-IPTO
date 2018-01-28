52837bb0f5758cfd31fead6572f3e41383b33ed4
Praxitelis-Nikolaos Kouroupetroglou <praxitelisk@gmail.com>
2018-01-28 14:57:53 +0200

    adding postprocess scripts

--- scripts/8.3-ipto_model_ensembling_feature_selection_model_selection.R
+++ scripts/8.3-ipto_model_ensembling_feature_selection_model_selection.R
@@ -5,39 +5,44 @@
 
 temp = 0
 str_pred_ensembling = c()
-str_model_ensembling = c()
 
 for(i in 1:24) {
   
-  str.model.select = c("svm.fs.ms", "nn.fs.ms", "xgboost.fs.ms", "randomForest.fs.ms", "rule.fs.ms", "knn.fs.ms")
-  str.pred.select = c("prediction.svm.fs.ms", "prediction.nn.fs.ms", "prediction.xgboost.fs.ms", "prediction.randomForest.fs.ms", "prediction.rule.fs.ms", "knn.fs.ms")
   
-  model.select = c(mape.svm.fs.ms[[i]], mape.nn.fs.ms[[i]], mape.xgboost.fs.ms[[i]], mape.randomForest.fs.ms[[i]] ,mape.rule.fs.ms[[i]], mape.knn.fs.ms[[i]])
-  pred.select = c(prediction.svm.fs.ms[[i]], prediction.nn.fs.ms[[i]], prediction.xgboost.fs.ms[[i]], prediction.randomForest.fs.ms[[i]], prediction.rule.fs.ms[[i]], prediction.knn.fs.ms[[i]])
+  str.model.select = c("nn", "svm", "xgboost", "rule", "randomForest", "knn")
   
   
-  #cat("best mape for ", i-1 ,"th hour: ", min(model.select), ", calculated from model: ", str.model.select[which.min(model.select)] ,", fit.", str.model.select[which.min(model.select)], ".", i-1, " prediction: ", str.pred.select[which.min(model.select)], ".", i-1, "\n", sep = "")
-  cat("best mape for ", i-1 ,"th hour: ", min(model.select), ", calculated from model: ", str.model.select[which.min(model.select)] ,", fit.", str.model.select[which.min(model.select)], ".", i-1, "\n", sep = "")
+  model.select = c(best.nn.parameters.fs[[i]][paste("mape.nn", i-1, sep = ".")],
+                   best.svm.parameters.fs[[i]][paste("mape.svm", i-1, sep = ".")],
+                   best.xgboost.parameters.fs[[i]][paste("mape.xgboost", i-1, sep = ".")],
+                   best.rule.parameters.fs[[i]][paste("mape.rule", i-1, sep = ".")],
+                   best.randomForest.parameters.fs[[i]][paste("mape.randomForest", i-1, sep = ".")],
+                   best.knn.parameters.fs[[i]][paste("mape.knn", i-1, sep = ".")])
   
-  str_model_ensembling = append(str_model_ensembling, str.model.select[which.min(model.select)])
+  
+  str.pred.select = c("prediction.nn.fs.ms", "prediction.svm.fs.ms", "prediction.xgboost.fs.ms", "prediction.rule.fs.ms", "prediction.randomForest.fs.ms", "prediction.knn.fs.ms")
+  
+  cat("best mape for ", i-1 ,"th hour: ", min(model.select), ", calculated from model: ", str.model.select[which.min(model.select)], " prediction: ", str.pred.select[which.min(model.select)], ".", i-1, "\n", sep = "")
+  
+  
+  
   str_pred_ensembling = append(str_pred_ensembling, str.pred.select[which.min(model.select)])
   
-  temp = temp + model.select[which.min(model.select)]
   
 }
 
-pred.ensembling.fs.ms = c(
+prediction.ensembling.fs.ms = c(
   rbind(
     get(str_pred_ensembling[1])[[1]],
     get(str_pred_ensembling[2])[[2]],
     get(str_pred_ensembling[3])[[3]],
     get(str_pred_ensembling[4])[[4]],
-    get(str_pred_ensembling[5])[[5]],
-    get(str_pred_ensembling[6])[[6]],
+    get(str_pred_ensembling[5])[[5]], #here it is a neural network, thus the special handling
+    get(str_pred_ensembling[6])[[6]][1:365, 1],
     get(str_pred_ensembling[7])[[7]],
     get(str_pred_ensembling[8])[[8]],
     get(str_pred_ensembling[9])[[9]],
-    get(str_pred_ensembling[10])[[10]],
+    get(str_pred_ensembling[10])[[10]][1:365, 1],
     get(str_pred_ensembling[11])[[11]],
     get(str_pred_ensembling[12])[[12]],
     get(str_pred_ensembling[13])[[13]],
@@ -56,11 +61,77 @@ pred.ensembling.fs.ms = c(
 )
 
 
-cat("\n\n mean mape with feature selection:", temp/24)
-cat("\n abbreviations: ms: model selection \n fs: feature selection\n")
+#cat("\n\n mean mape with feature selection:", temp/24)
+#cat("\n abbreviations:\n ms: model selection \n fs: feature selection\n")
 
 
-rm(temp, str.model.select, str.pred.select, model.select, pred.select, str_model_ensembling, str_pred_ensembling)
+
+###############################################################
+##########old code############################################
+###############################################################
+#str_model_ensembling = c()
+
+#for(i in 1:24) {
 
 
-#elapsed time in minutes: 8.932833
+
+#str.model.select = c("svm.fs.ms", "rule.fs.ms", "xgboost.fs.ms", "knn.fs.ms", "randomForest.fs.ms", "nn.fs.ms")
+#str.pred.select = c("prediction.svm.fs.ms", "prediction.rule.fs.ms", "prediction.xgboost.fs.ms", "prediction.knn.fs.ms", "prediction.randomForest.fs.ms", "prediction.nn.fs.ms")
+
+#model.select = c(mape.svm.fs.ms[[i]], mape.rule.fs.ms[[i]], mape.xgboost.fs.ms[[i]], mape.knn.fs.ms[[i]], mape.randomForest.fs.ms[[i]], mape.nn.fs.ms[[i]])
+#pred.select = c(prediction.svm.fs.ms[[i]], prediction.rule.fs.ms[[i]], prediction.xgboost.fs.ms[[i]], prediction.knn.fs.ms[[i]], prediction.randomForest.fs.ms[[i]], prediction.nn.fs.ms[[i]])
+
+
+#str.model.select = c("rule.fs.ms")
+#str.pred.select = c("prediction.rule.fs.ms")
+
+#model.select = c(mape.rule.fs.ms[[i]])
+#pred.select = c(prediction.rule.fs.ms[[i]])
+
+
+#cat("best mape for ", i-1 ,"th hour: ", min(model.select), ", calculated from model: ", str.model.select[which.min(model.select)] ,", fit.", str.model.select[which.min(model.select)], ".", i-1, " prediction: ", str.pred.select[which.min(model.select)], ".", i-1, "\n", sep = "")
+#cat("best mape for ", i-1 ,"th hour: ", min(model.select), ", calculated from model: ", str.model.select[which.min(model.select)] ,", fit.", str.model.select[which.min(model.select)], ".", i-1, "\n", sep = "")
+
+#str_model_ensembling = append(str_model_ensembling, str.model.select[which.min(model.select)])
+#str_pred_ensembling = append(str_pred_ensembling, str.pred.select[which.min(model.select)])
+
+#temp = temp + model.select[which.min(model.select)]
+
+#}
+
+# pred.ensembling.fs.ms = c(
+#   rbind(
+#     get(str_pred_ensembling[1])[[1]],
+#     get(str_pred_ensembling[2])[[2]],
+#     get(str_pred_ensembling[3])[[3]],
+#     get(str_pred_ensembling[4])[[4]],
+#     get(str_pred_ensembling[5])[[5]],
+#     get(str_pred_ensembling[6])[[6]],
+#     get(str_pred_ensembling[7])[[7]],
+#     get(str_pred_ensembling[8])[[8]],
+#     get(str_pred_ensembling[9])[[9]],
+#     get(str_pred_ensembling[10])[[10]],
+#     get(str_pred_ensembling[11])[[11]],
+#     get(str_pred_ensembling[12])[[12]],
+#     get(str_pred_ensembling[13])[[13]],
+#     get(str_pred_ensembling[14])[[14]],
+#     get(str_pred_ensembling[15])[[15]],
+#     get(str_pred_ensembling[16])[[16]],
+#     get(str_pred_ensembling[17])[[17]],
+#     get(str_pred_ensembling[18])[[18]],
+#     get(str_pred_ensembling[19])[[19]],
+#     get(str_pred_ensembling[20])[[20]],
+#     get(str_pred_ensembling[21])[[21]],
+#     get(str_pred_ensembling[22])[[22]],
+#     get(str_pred_ensembling[23])[[23]],
+#     get(str_pred_ensembling[24])[[24]]
+#   )
+# )
+# 
+# 
+# cat("\n\n mean mape with feature selection:", temp/24)
+# cat("\n abbreviations: ms: model selection \n fs: feature selection\n")
+# 
+# 
+# rm(temp, str.model.select, str.pred.select, model.select, pred.select, str_model_ensembling, str_pred_ensembling)
+
--- scripts/9.1-predictions_postprocess.R
+++ scripts/9.1-predictions_postprocess.R
@@ -20,15 +20,73 @@ time =
 loads = darkSky.N.Loads.Combined$Loads[which(darkSky.N.Loads.Combined$time == testSet$time[1]):nrow(darkSky.N.Loads.Combined)]
 
 
+mean.mape.ensembling.fs.ms =  100 * mean(abs((loads - prediction.ensembling.fs.ms)/loads))
 
-df = data.frame("time" = time, "loads" = loads, "prediction.ensembling" = pred.ensembling.fs.ms)
+
+select_mape = c(mean.mape.svm.fs.ms, 
+                mean.mape.xgboost.fs.ms, 
+                mean.mape.rule.fs.ms, 
+                mean.mape.nn.fs.ms, 
+                mean.mape.randomForest.fs.ms,
+                mean.mape.knn.fs.ms,
+                mean.mape.ensembling.fs.ms)
+
+
+select_mape_str = c("svm", "xgboost", "rule", "nn", "randomForest", "knn", "ensembling")
+
+
+#need to add more if-else when nn or ensembling is selected
+if (select_mape_str[which.min(select_mape)] == "svm" | 
+    select_mape_str[which.min(select_mape)] == "xgboost" | 
+    select_mape_str[which.min(select_mape)] == "rule" | 
+    select_mape_str[which.min(select_mape)] == "nn" | 
+    select_mape_str[which.min(select_mape)] == "randomForest") {
+  
+  
+  prediction = c(
+    rbind(
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[1]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[2]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[3]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[4]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[5]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[6]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[7]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[8]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[9]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[10]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[11]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[12]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[13]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[14]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[15]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[16]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[17]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[18]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[19]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[20]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[21]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[22]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[23]],
+      get(paste("prediction", select_mape_str[which.min(select_mape)], "fs.ms", sep = "."))[[24]]
+    )
+  )
+  
+  
+}
+
+
+
+df = data.frame("time" = time, "loads" = loads, "prediction" = prediction)
 pred.ensembling.df = df
+
 
 
 x <- seq(
   as.Date(as.POSIXct(min(pred.ensembling.df$time), tz= "Europe/Athens"), tz= "Europe/Athens"), 
   as.Date(as.POSIXct(max(pred.ensembling.df$time), tz= "Europe/Athens"), tz= "Europe/Athens"),
   by="1 day")
+
 
 
 OctoberToBeAdded = x[wday(x,label = TRUE) == "Sun" & day(x) >= 25 & month(x) == 10]
@@ -83,12 +141,12 @@ pred.ensembling.df$ooem.predictions = ooem_predictions$ooem_predictions
 
 rownames(pred.ensembling.df) = 1:dim(pred.ensembling.df)[1]
 
-mape.postprocessed = 100 * mean(abs((pred.ensembling.df$loads - pred.ensembling.df$prediction.ensembling)/pred.ensembling.df$loads))
+mape.postprocessed = 100 * mean(abs((pred.ensembling.df$loads - pred.ensembling.df$prediction)/pred.ensembling.df$loads))
 
 mape.ooem = 100 * mean(abs((pred.ensembling.df$loads - pred.ensembling.df$ooem.predictions)/pred.ensembling.df$loads))
 
 
-pred.ensembling.df["mape"]  = 100 * abs((pred.ensembling.df$loads - pred.ensembling.df$prediction.ensembling)/pred.ensembling.df$loads)
+pred.ensembling.df["mape"]  = 100 * abs((pred.ensembling.df$loads - pred.ensembling.df$prediction)/pred.ensembling.df$loads)
 pred.ensembling.df["mape.ooem"]  = 100 * abs((pred.ensembling.df$loads - pred.ensembling.df$ooem.predictions)/pred.ensembling.df$loads)
 
 #print results#########
